3b73a6309e5b853c30229d9ac2e33d4781a9d8eb
Alameen456 <kareemalameen291@gmail.com>
2026-04-28 12:55:28 +0100

    My assignment

---
+++ Alameen/dom.js
@@ -0,0 +1,13 @@
+let result = 0;
+
+function increase() {
+    result ++;
+    document.getElementById("number").textContent = result;
+};
+
+
+  //decrement
+function decrease() {
+    result--;
+        document.getElementById("number").textContent = result;
+    }
