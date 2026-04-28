837be574925106d8a2544a986f46a3d7989c1163
Monas Grey <nasirumoridiyat@gmail.com>
2026-04-28 12:24:57 +0100

    grey

---
+++ greys assignment/dom.js
@@ -0,0 +1,18 @@
+let result = 0;
+
+function increase() {
+    result++;
+    document.getElementById('number').textContent = result;
+}
+
+function decrease() {
+    result--;
+    document.getElementById('number').textContent = result;
+}
+
+function addtwo() {
+    result +=2 ;
+    document.getElementById('number').textContent = result;
+}
+
+
